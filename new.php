e5274ded2eb80c1ee2593b557b39f1366ddd2f99
Jonas <jonas.2022310916@aluno.iffar.edu.br>
2024-03-04 16:57:13 -0300

    php novos e modificados

--- new.php
+++ new.php
@@ -1,3 +1,29 @@
 <?php
-$i=10;
+if($_POST)
+{
+
+}
+else{
+    
+}
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ínicio</title>
+</head>
+<body>
+   <form method="POST">
+    <fieldset>
+        <legend><h1>
+            Registrar Usuario
+        </h1></legend>
+        Email: <input type="text" name="email"><br>
+        Senha: <input type="text" name="senha">
+    </fieldset>
+    <input type="submit" value="Registrar">
+   </form>
+</body>
+</html>
